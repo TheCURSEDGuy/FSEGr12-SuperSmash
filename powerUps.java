8ba2f3ad63bda4da7b02ab95d6521c948217ad43
alhoussi <50009108@student.publicboard.ca>
2023-06-09 13:48:14 -0400

    powerups

---
+++ powerUps.java
@@ -0,0 +1,23 @@
+import java.awt.Rectangle;
+import java.util.Spliterators.AbstractIntSpliterator;
+
+
+
+public class powerUps {
+    public int width, height, player;
+
+    public powerUps(int ww, int hh, int pp){
+        width = ww;
+        height = hh;
+        player = pp;
+    }
+
+    public void multiHit(player bigSpoon, player littleSpoon){
+        //check if sword rectangle intersects player 
+
+
+        Rectangle swordieSlash = new Rectangle(bigSpoon.getRect().x, bigSpoon.getRect().y, 2*bigSpoon.getRect().x, bigSpoon.getRect().y);
+        
+    }
+
+}
